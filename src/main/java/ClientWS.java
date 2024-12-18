8674cbd2cb3cee1b3163b437eca8eed8c3893a4d
md-naciri <med.ar.naciri@gmail.com>
2024-12-18 22:13:03 +0100

    V1

---
+++ src/main/java/ClientWS.java
@@ -0,0 +1,24 @@
+import proxy.BanqueService;
+import proxy.BanqueWS;
+import proxy.Compte;
+
+import java.util.List;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService proxy = new BanqueWS().getBanqueServicePort();
+        System.out.println(proxy.convert(500));
+        Compte cp = proxy.getCompte(5);
+        System.out.println("-------------------------");
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+        System.out.println("-------------------------");
+        List<Compte> comptes = proxy.listComptes();
+        comptes.forEach(c -> {
+            System.out.println("-------------------------");
+            System.out.println(c.getCode());
+            System.out.println(c.getSolde());
+            System.out.println("-------------------------");
+        });
+    }
+}
